e82f0337994008a84f2b5e35c9f94160da3dba29
JasonPaulino <jasonp0830@gmail.com>
2023-05-15 14:21:33 -0400

    added a new book moved books info to a model.js

---
+++ tutorial/src/model.js
@@ -0,0 +1,23 @@
+const books = [
+  {
+    id: 1,
+    img: "/public/giraffe.jpg",
+    title:
+      "There Are Moms Way Worse Than You: Irrefutable Proof That You Are Indeed a Fantastic Parent",
+    author: "Gleen Boozaman",
+  },
+  {
+    id: 2,
+    img: "/public/drSeuss.jpg",
+    title: "Oh, the Places You'll Go!",
+    author: "Dr. Seuss",
+  },
+  {
+    id: 2,
+    img: "/public/hello-beautiful.jpg",
+    title: "Hello Beautiful: A Novel",
+    author: "ANn Napolitano"
+  }
+];
+
+export default books;
